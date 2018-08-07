8d1ded0f66120124982031bd68f05546b6936a9b
josecaos <jazzvoon@gmail.com>
2018-08-06 22:25:29 -0500

    debug

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -7,12 +7,14 @@
 * @package Tyche
 */
 
+global $product
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'tyche-blog-post' ); ?>>
 	<header class="entry-header">
 		<div class="tyche-blog-meta">
-			<?php Tyche_Helper::post_meta(); ?>
+			<?php //Tyche_Helper::post_meta(); ?>
+			<?php echo get_the_post_title(); ?>
 		</div>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
@@ -25,6 +27,14 @@
 			<hr>
 			<div class="col-md-9 col-lg-10">
 			<?php the_excerpt(); ?>
+			<div id="product-description-container" style="border:2px dashed green;">
+  <ul>
+  <a href="<?php echo esc_url( get_permalink( $product->id ) ); ?>" title="<?php echo esc_attr( $product->get_title() ); ?>">
+    <li><h4><?php echo $product->get_title(); ?></h4></li></a>
+    <li><?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt )?></li>
+    <li><h6><?php echo $product->get_price_html(); ?>  **MISSING CODE TO ADD TO CART BUTTON HERE**</h6></li>
+ </ul>
+</div>
 		</div>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
