505cfb4e1450f9caf3e41b390ab5cb15e2bc7268
josecaos <jazzvoon@gmail.com>
2018-12-03 18:40:10 -0600

    Fix a plantilla ofertas

--- sale.php
+++ sale.php
@@ -4,39 +4,55 @@ Template Name: Ofertas ArmarioJoyeria
 */
 get_header();
 ?>
-<!-- WooCommerce On-Sale Products -->
-<ul class="products">
+<div class="container-fluid">
+
+  <ul class="row products">
+
     <?php
-        $args = array(
-            'post_type'      => 'product',
-            'posts_per_page' => 4,
-            'meta_query'     => array(
-                    'relation' => 'OR',
-                    array( // Simple products type
-                        'key'           => '_sale_price',
-                        'value'         => 0,
-                        'compare'       => '>',
-                        'type'          => 'numeric'
-                    ),
-                    array( // Variable products type
-                        'key'           => '_min_variation_sale_price',
-                        'value'         => 0,
-                        'compare'       => '>',
-                        'type'          => 'numeric'
-                    )
-                )
-        );
-        $loop = new WP_Query( $args );
-        if ( $loop->have_posts() ) {
-            while ( $loop->have_posts() ) : $loop->the_post();
-                woocommerce_get_template_part( 'content', 'product' );
-            endwhile;
-        } else {
-            echo __( 'No products found' );
-        }
-        wp_reset_postdata();
-    ?>
+    $args = array(
+      'post_type'      => 'product',
+      'posts_per_page' => 4,
+      'meta_query'     => array(
+        'relation' => 'OR',
+        array( // Simple products type
+          'key'           => '_sale_price',
+          'value'         => 0,
+          'compare'       => '>',
+          'type'          => 'numeric'
+        ),
+        array( // Variable products type
+          'key'           => '_min_variation_sale_price',
+          'value'         => 0,
+          'compare'       => '>',
+          'type'          => 'numeric'
+        )
+      )
+    );
+    $loop = new WP_Query( $args );
+    if ( $loop->have_posts() ) {
+      while ( $loop->have_posts() ) : $loop->the_post();
+      ?>
+
+      <li class="col-12 col-sm-6 col-lg-3">
+
+
+      <?php
+      woocommerce_get_template_part( 'content', 'product' );
+      ?>
+
+    </li>
+
+
+      <?php
+
+    endwhile;
+  } else {
+    echo __( 'No products found' );
+  }
+  wp_reset_postdata();
+  ?>
 </ul>
+</div>
 
 <?php get_footer(); ?>
 <!-- WooCommerce On-Sale Products -->
