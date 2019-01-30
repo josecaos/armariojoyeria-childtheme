7a241dfe96051c7d977bc292aa4ebc3c01ad97f6
josecaos <jazzvoon@gmail.com>
2019-01-30 12:14:06 -0600

    Add FB Pixel

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 *
 * @package Tyche
 */
-
+F
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -23,17 +23,33 @@
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-K4WJQMN');</script>
 <!-- End Google Tag Manager -->
-
-<?php wp_head(); ?>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '1407055999305968');
+	fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=1407055999305968&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 	<!-- rebajas floating button -->
-<div id="sale_floating" class="text-center hidden">
-	<a href="https://armariojoyeria.com/rebajas">
-		<i class="fa fa-gift"></i> Rebajas
-	</a>
-</div>
+	<div id="sale_floating" class="text-center hidden">
+		<a href="https://armariojoyeria.com/rebajas">
+			<i class="fa fa-gift"></i> Rebajas
+		</a>
+	</div>
 	<!--  -->
 
 	<!-- Google Tag Manager (noscript) -->
