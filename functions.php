b15820d8797570ee6b1b0ec5e8951d5b5bf4f649
josecaos <jazzvoon@gmail.com>
2019-02-01 13:57:12 -0600

    Agrega codigo en storefront

--- functions.php
+++ functions.php
@@ -203,3 +203,21 @@ function arma_tu_accesorio_single_product( $template ) {
     }
     return $template;
 }
+
+add_action( 'storefront_header', 'storefront_header_content', 40 );
+function storefront_header_content() { ?>
+  <!-- Facebook Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  	n.queue=[];t=b.createElement(e);t.async=!0;
+  	t.src=v;s=b.getElementsByTagName(e)[0];
+  	s.parentNode.insertBefore(t,s)}(window, document,'script',
+  	'https://connect.facebook.net/en_US/fbevents.js');
+  	fbq('init', '1407055999305968');
+  	fbq('track', 'PageView');
+  </script>
+	<?php
+}
