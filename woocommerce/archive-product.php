ad58fd1c004442b9bb592cb732943d0a9b8df7e3
josecaos <jazzvoon@gmail.com>
2019-02-27 12:45:03 -0600

    fixes a header birthstones

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see         https://docs.woocommerce.com/document/template-structure/
- * @author      WooThemes
- * @package     WooCommerce/Templates
- * @version     3.3.0
- */
+* The Template for displaying product archives, including the main shop page which is a post type archive
+*
+* This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+*
+* HOWEVER, on occasion WooCommerce will need to update template files and you
+* (the theme developer) will need to copy the new files to your theme to
+* maintain compatibility. We try to do this as little as possible, but it does
+* happen. When this occurs the version of the template file will be bumped and
+* the readme will list any important changes.
+*
+* @see         https://docs.woocommerce.com/document/template-structure/
+* @author      WooThemes
+* @package     WooCommerce/Templates
+* @version     3.3.0
+*/
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -27,12 +27,12 @@ get_header( 'shop' ); ?>
 		<div class="col-md-12">
 			<?php
 			/**
-			 * woocommerce_before_main_content hook.
-			 *
-			 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-			 * @hooked woocommerce_breadcrumb - 20
-			 * @hooked WC_Structured_Data::generate_website_data() - 30
-			 */
+			* woocommerce_before_main_content hook.
+			*
+			* @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+			* @hooked woocommerce_breadcrumb - 20
+			* @hooked WC_Structured_Data::generate_website_data() - 30
+			*/
 			do_action( 'woocommerce_before_main_content' );
 			?>
 		</div>
@@ -42,10 +42,10 @@ get_header( 'shop' ); ?>
 			<div class="col-md-4 hidden-xs">
 				<?php
 				/**
-				 * woocommerce_sidebar hook.
-				 *
-				 * @hooked woocommerce_get_sidebar - 10
-				 */
+				* woocommerce_sidebar hook.
+				*
+				* @hooked woocommerce_get_sidebar - 10
+				*/
 				do_action( 'woocommerce_sidebar' );
 				?>
 			</div>
@@ -53,11 +53,11 @@ get_header( 'shop' ); ?>
 		<div class="<?php echo ( is_active_sidebar( 'shop-sidebar' ) && 'fullwidth' !== $layout ) ? 'col-md-8 tyche-has-sidebar' : 'col-md-12'; ?>">
 			<?php
 			/**
-			 * woocommerce_archive_description hook.
-			 *
-			 * @hooked woocommerce_taxonomy_archive_description - 10
-			 * @hooked woocommerce_product_archive_description - 10
-			 */
+			* woocommerce_archive_description hook.
+			*
+			* @hooked woocommerce_taxonomy_archive_description - 10
+			* @hooked woocommerce_product_archive_description - 10
+			*/
 			do_action( 'woocommerce_archive_description' );
 			?>
 
@@ -72,25 +72,29 @@ get_header( 'shop' ); ?>
 
 				<?php
 				/**
-				 * woocommerce_before_shop_loop hook.
-				 *
-				 * @hooked wc_print_notices - 10
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
+				* woocommerce_before_shop_loop hook.
+				*
+				* @hooked wc_print_notices - 10
+				* @hooked woocommerce_result_count - 20
+				* @hooked woocommerce_catalog_ordering - 30
+				*/
 				do_action( 'woocommerce_before_shop_loop' );
 				//
 				// banner archivo / categoria
 				if(is_product_category('birth-stones')):
-				?>
-				<div class="col-12 col-sm-8 archive_header_img imgLiquid imgLiquidNoFill">
-					<img src="https://armariojoyeria.com/wp-content/uploads/2019/02/slider-coleccion-birthstones.png" alt="">
-				</div>
-				<div class="col-12 col-sm-8">
-				<div class="row text-center">
-					<a href="<?php echo get_page_link(93) ?>" class="col-auto my-auto">Collares</a>
-				</div>
-				</div>
+					?>
+					<div class="col-12 archive_header_img imgLiquid imgLiquidNoFill">
+						<div class="col-12 col-md-8">
+							<img src="https://armariojoyeria.com/wp-content/uploads/2019/02/slider-coleccion-birthstones.png" alt="">
+						</div>
+
+						<div class="col-12 col-md-4">
+							<div class="row text-center">
+								<a href="<?php echo get_page_link(93) ?>" class="col-auto my-auto">Collares</a>
+							</div>
+						</div>
+					</div>
+
 
 				<?php endif; ?>
 
@@ -103,10 +107,10 @@ get_header( 'shop' ); ?>
 						the_post();
 
 						/**
-						 * Hook: woocommerce_shop_loop.
-						 *
-						 * @hooked WC_Structured_Data::generate_product_data() - 10
-						 */
+						* Hook: woocommerce_shop_loop.
+						*
+						* @hooked WC_Structured_Data::generate_product_data() - 10
+						*/
 						do_action( 'woocommerce_shop_loop' );
 
 						wc_get_template_part( 'content', 'product' );
@@ -118,10 +122,10 @@ get_header( 'shop' ); ?>
 
 				<?php
 				/**
-				 * woocommerce_after_shop_loop hook.
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
+				* woocommerce_after_shop_loop hook.
+				*
+				* @hooked woocommerce_pagination - 10
+				*/
 				do_action( 'woocommerce_after_shop_loop' );
 				?>
 
@@ -129,10 +133,10 @@ get_header( 'shop' ); ?>
 
 				<?php
 				/**
-				 * woocommerce_no_products_found hook.
-				 *
-				 * @hooked wc_no_products_found - 10
-				 */
+				* woocommerce_no_products_found hook.
+				*
+				* @hooked wc_no_products_found - 10
+				*/
 				do_action( 'woocommerce_no_products_found' );
 				?>
 
@@ -142,20 +146,20 @@ get_header( 'shop' ); ?>
 			<div class="col-md-4 hidden-xs">
 				<?php
 				/**
-				 * woocommerce_sidebar hook.
-				 *
-				 * @hooked woocommerce_get_sidebar - 10
-				 */
+				* woocommerce_sidebar hook.
+				*
+				* @hooked woocommerce_get_sidebar - 10
+				*/
 				do_action( 'woocommerce_sidebar' );
 				?>
 			</div>
 		<?php endif; ?>
 		<?php
 		/**
-		 * woocommerce_after_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
+		* woocommerce_after_main_content hook.
+		*
+		* @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		*/
 		do_action( 'woocommerce_after_main_content' );
 		?>
 	</div>
